2b4b0abb66e1694c11875d00838339e051283c4f
Natanael Silva <53065432+natanaelsc96@users.noreply.github.com>
2022-07-01 17:18:21 -0300

    refactor: add EmptyResult exception in getOne category

--- backend/src/main/java/br/com/moneyapi/service/CategoryService.java
+++ backend/src/main/java/br/com/moneyapi/service/CategoryService.java
@@ -3,6 +3,7 @@ package br.com.moneyapi.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.moneyapi.model.Category;
@@ -23,6 +24,6 @@ public class CategoryService {
     }
 
     public Category getOne(Long code) {
-        return categoryRepository.findById(code).orElse(null);
+        return categoryRepository.findById(code).orElseThrow(() -> new EmptyResultDataAccessException(1));
     }
 }
