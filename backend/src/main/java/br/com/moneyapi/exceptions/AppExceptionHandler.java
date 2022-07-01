534fb4d75fcc5e98a7846116f03dc99c8c4e7497
Natanael Silva <53065432+natanaelsc96@users.noreply.github.com>
2022-07-01 16:00:19 -0300

    refactor: rename ExceptionHandler to AppExceptionHandler and add EmptyResultException

--- backend/src/main/java/br/com/moneyapi/exceptions/AppExceptionHandler.java
+++ backend/src/main/java/br/com/moneyapi/exceptions/AppExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
     
     @Autowired
     private MessageSource messageSource;
@@ -48,6 +50,14 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
             errors.add(new Erro(userMessage, developerMessage));
         }
         return errors;
+    }
+
+    @ExceptionHandler({ EmptyResultDataAccessException.class })
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+        String userMessage = messageSource.getMessage("resource.not-found", null, LocaleContextHolder.getLocale());
+        String developerMessage = ex.toString();
+        List<Erro> errors = Arrays.asList(new Erro(userMessage, developerMessage));
+        return super.handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     public static class Erro {
