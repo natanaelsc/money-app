47ef082ba3bce5e39d6ce8561fdf8efcbbfd68b9
Natanael Silva <53065432+natanaelsc96@users.noreply.github.com>
2022-07-03 14:49:51 -0300

    chore: remove obsolete entire display width

--- backend/src/main/resources/db/migration/V04__create_entry_table.sql
+++ backend/src/main/resources/db/migration/V04__create_entry_table.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS `entry` 
 (
-	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
 	`description` VARCHAR(50) NOT NULL,
 	`due_date` DATE NOT NULL,
 	`payment_date` DATE,
 	`amount` DECIMAL(10,2) NOT NULL,
 	`note` VARCHAR(100),
 	`type` VARCHAR(20) NOT NULL,
-	`category_id` BIGINT(20),
-	`person_id` BIGINT(20),
+	`category_id` BIGINT,
+	`person_id` BIGINT,
     CONSTRAINT `entry_pkey` PRIMARY KEY (`id`),
 	FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
 	FOREIGN KEY (`person_id`) REFERENCES `person`(`id`)
