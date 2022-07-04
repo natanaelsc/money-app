811e9ea1b5c9568d376151c5a85cc450b9fe36a9
Natanael Silva <53065432+natanaelsc96@users.noreply.github.com>
2022-07-04 11:22:41 -0300

    refactor: add @notnull

--- backend/src/main/java/br/com/moneyapi/model/Entry.java
+++ backend/src/main/java/br/com/moneyapi/model/Entry.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -25,25 +26,31 @@ public class Entry {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotNull
 	private String description;
 
+	@NotNull
 	@Column(name = "due_date")
 	private LocalDate dueDate;
-
+	
 	@Column(name = "payment_date")
 	private LocalDate paymentDate;
 
+	@NotNull
 	private BigDecimal amount;
 
 	private String note;
 
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private EntryType type;
 
+	@NotNull(message = "Category is required")
 	@ManyToOne
 	@JoinColumn(name = "category_id")
 	private Category category;
 
+	@NotNull(message = "Person is required")
 	@ManyToOne
 	@JoinColumn(name = "person_id")
 	private Person person;
